680c4535b0d13643f13c6b78b0e70a8e1f380dd2
Oscar Luu <oscarluu26@gmail.com>
2019-01-31 00:29:01 -0800

    Add more page modal

--- src/more.js
+++ src/more.js
@@ -6,27 +6,85 @@ import Modal from "react-modal";
 const beginURL = "https://dog.ceo/api/breed/";
 const endURL = "/images";
 
-const ImageLoad = props => {
-  return (
-    <div>
-      <p className="title-more">
-        Pictures of{" "}
-        {props.breed.slice(0, 1).toUpperCase() +
-          props.breed.slice(1, props.breed.length)}
-      </p>
-      <div className="image-gallery">
-        {props.data.slice(0, props.limit).map(function(data) {
-          return (
-            <div className="image-gallery">
-              <img src={data} className="more-image" />
-            </div>
-          );
-        })}
+// const ImageLoad = props => {
+//   return (
+//     <div>
+//       <p className="title-more">
+//         Pictures of{" "}
+//         {props.breed.slice(0, 1).toUpperCase() +
+//           props.breed.slice(1, props.breed.length)}
+//         s
+//       </p>
+//       <div className="image-gallery">
+//         {props.data.slice(0, props.limit).map(function(data) {
+//           return (
+//             <div className="image-gallery">
+//               <img src={data} className="more-image" />
+//             </div>
+//           );
+//         })}
+//       </div>
+//       <div className="button-center">
+//         <button onClick={props.onClick} className="button">
+//           Load More
+//         </button>
+//       </div>
+//     </div>
+//   );
+// };
+
+class ImageLoad extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showModal: false
+    };
+    this.handleCloseModal = this.handleCloseModal.bind(this);
+    this.handleOpenModal = this.handleOpenModal.bind(this);
+  }
+
+  handleOpenModal() {
+    this.setState({ showModal: true });
+  }
+
+  handleCloseModal() {
+    this.setState({ showModal: false });
+  }
+
+  render() {
+    return (
+      <div>
+        <p className="title-more">
+          Pictures of{" "}
+          {this.props.breed.slice(0, 1).toUpperCase() +
+            this.props.breed.slice(1, this.props.breed.length)}
+          s
+        </p>
+        <div className="image-gallery">
+          {this.props.data.slice(0, this.props.limit).map(data => {
+            return (
+              <div className="image-gallery">
+                <img
+                  src={data}
+                  className="more-image"
+                  onClick={this.handleOpenModal}
+                />
+                <Modal isOpen={this.state.showModal}>
+                  <img src={data} onClick={this.handleCloseModal} />
+                </Modal>
+              </div>
+            );
+          })}
+        </div>
+        <div className="button-center">
+          <button onClick={this.props.onClick} className="button">
+            Load More
+          </button>
+        </div>
       </div>
-      <button onClick={props.onClick}>Load More</button>
-    </div>
-  );
-};
+    );
+  }
+}
 
 class More extends Component {
   constructor(props) {
